23ef74f7de6a8ec5629ee26c2ae41bf4b0edf375
septiady <cit.septiyadi.dev@gmail.com>
2022-01-03 11:05:09 +0700

    Add first project Android Kotlin

---
+++ app/src/main/java/dev/adiroot/andkot/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.adiroot.andkot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
